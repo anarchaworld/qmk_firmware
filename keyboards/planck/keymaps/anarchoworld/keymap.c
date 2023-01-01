ec4d5388157b6893dfea42d3909cfdf659639c6d
Ash <86912625+anarchaworld@users.noreply.github.com>
2022-12-31 20:25:26 -0800

    fixed

--- keyboards/planck/keymaps/anarchoworld/keymap.c
+++ keyboards/planck/keymaps/anarchoworld/keymap.c
@@ -71,6 +71,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_Q,         KC_W,         KC_F,         KC_P,         KC_B,         _,             _,             KC_J,         KC_L,         KC_U,         KC_Y,         QUOT,
             KC_A,         KC_R,         KC_S,         KC_T,         KC_G,         _,             _,             KC_M,         KC_N,         KC_E,         KC_I,         KC_O,
             KC_Z,         KC_X,         KC_C,         KC_D,         KC_V,         _,             _,             KC_K,         KC_H,         COMM,         DOT,          SLSH,
-            _,            _,            ESC,          SPC,          TAB,                         _,             ENT,          BSPC,         DEL,          _,            TG(BASE)),
+            KC_LALT,      _,            ESC,          SPC,          TAB,                         _,             ENT,          BSPC,         DEL,          _,            TG(GME)),
 
 };
